4184184f4a2dcbbea5e404f8c9719e047a2f23ed
rabucejo <rogerhapay@gmail.com>
2023-05-26 09:06:27 +0800

    add delete function

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -132,5 +132,49 @@
             $('.filedesc').val(FileDescription);
             $('.filepath').val(FilePath);
         }
+        function delete(FileId){
+        var file_Data = new FormData()
+        file_Data.append('StudentID',FileId)
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                $.ajax({
+                    method: "POST",
+                    url: "{{route('delete')}}",
+                    dataType: 'json',
+                    processData:false,
+                    contentType:false,
+                    cache:false,
+                    async:false,
+                    data: file_Data,
+                }).done(function( msg ) {
+                    if(msg.result ==true){
+                        Swal.fire(
+                        'Delete',
+                        msg.message,
+                        'success'
+                        )
+                        setTimeout(function(){
+                        window.location.reload();
+                        }, 2000);
+                    } else {
+                        Swal.fire(
+                        'Delete',
+                        msg.message,
+                        'error'
+                        )
+                    }
+                });
+            }
+        })
+    }
     </script>
 @endsection
