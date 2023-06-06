423a59550347e8702c2b2d3abcb77ebf57960f5d
rabucejo <rogerhapay@gmail.com>
2023-06-06 12:05:49 +0800

    change function position

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -125,6 +125,53 @@
             });
         });
 
+        
+        function delete() {
+            var file_Data = new FormData()
+            file_Data.append('FileId', FileId)
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+
+                    $.ajax({
+                        method: "POST",
+                        url: "{{ route('delete') }}",
+                        dataType: 'json',
+                        processData: false,
+                        contentType: false,
+                        cache: false,
+                        async: false,
+                        data: file_Data,
+                    }).done(function(msg) {
+                        if (msg.result == true) {
+                            Swal.fire(
+                                'Delete',
+                                msg.message,
+                                'success'
+                            )
+                            setTimeout(function() {
+                                window.location.reload();
+                            }, 2000);
+                        } else {
+                            Swal.fire(
+                                'Delete',
+                                msg.message,
+                                'error'
+                            )
+                        }
+                    });
+                }
+            })
+            console.log('testing');
+        }
+
         // SHOW EDIT MODAL
         function edit(FileFolder, Filename, FileDescription, FilePath) {
             // get inputs & display to modal fields
@@ -134,51 +181,5 @@
             $('.filedesc').val(FileDescription);
             $('.filepath').val(FilePath);
         }
-
-        // function delete() {
-        //     var file_Data = new FormData()
-        //     file_Data.append('FileId', FileId)
-        //     Swal.fire({
-        //         title: 'Are you sure?',
-        //         text: "You won't be able to revert this!",
-        //         icon: 'warning',
-        //         showCancelButton: true,
-        //         confirmButtonColor: '#3085d6',
-        //         cancelButtonColor: '#d33',
-        //         confirmButtonText: 'Yes, delete it!'
-        //     }).then((result) => {
-        //         if (result.isConfirmed) {
-
-        //             $.ajax({
-        //                 method: "POST",
-        //                 url: "{{ route('delete') }}",
-        //                 dataType: 'json',
-        //                 processData: false,
-        //                 contentType: false,
-        //                 cache: false,
-        //                 async: false,
-        //                 data: file_Data,
-        //             }).done(function(msg) {
-        //                 if (msg.result == true) {
-        //                     Swal.fire(
-        //                         'Delete',
-        //                         msg.message,
-        //                         'success'
-        //                     )
-        //                     setTimeout(function() {
-        //                         window.location.reload();
-        //                     }, 2000);
-        //                 } else {
-        //                     Swal.fire(
-        //                         'Delete',
-        //                         msg.message,
-        //                         'error'
-        //                     )
-        //                 }
-        //             });
-        //         }
-        //     })
-        //     console.log('testing');
-        // }
     </script>
 @endsection
