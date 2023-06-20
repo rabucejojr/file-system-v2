50d790dbcfc9cb25bec5c6904c11e854bf9e5474
rabucejo <rogerhapay@gmail.com>
2023-06-20 08:57:23 +0800

    added breze auth

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/app', function () {
-    return view('partials.app');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/table', [FileController::class, 'table'])->name('table');
-Route::get('/dashboard', [FileController::class, 'dashboard'])->name('dashboard');
-Route::get('/upload', [FileController::class, 'upload'])->name('upload');
-Route::post('/store', [FileController::class, 'store'])->name('store');
-Route::post('/file/delete', [FileController::class, 'delete'])->name('delete');
-
-Route::get('/table2', [FileController::class, 'table2'])->name('file.table2');
-Route::get('/fetch-files', [FileController::class, 'fetchFiles'])->name('file.fetch');
-Route::get('/file/{id}/edit', [FileController::class, 'edit'])->name('file.edit');
-Route::post('/file/update/{id}', [FileController::class, 'update'])->name('file.update');
-Route::delete('/file/delete/{id}', [FileController::class, 'destroy'])->name('file.destroy');
+require __DIR__.'/auth.php';
