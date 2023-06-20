f427d07fe5e6a1e7b232473b8b49b9f60a96b2f6
rabucejo <rogerhapay@gmail.com>
2023-06-20 10:38:53 +0800

    added auth and route for login and register

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/register', function () {
+    return view('auth.register');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
