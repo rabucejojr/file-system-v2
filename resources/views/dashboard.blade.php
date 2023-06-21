82c54e5c67ebd38e18e3357f3e257183ba9bb778
rabucejo <rogerhapay@gmail.com>
2023-06-21 20:01:23 +0800

    modified dashboard displays

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -169,8 +169,8 @@
                         <div class="card shadow mb-4">
                             <!-- Card Header - Dropdown -->
                             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-primary">Priority Sectors</h6>
-                                <div class="dropdown no-arrow">
+                                <h6 class="m-0 font-weight-bold text-primary">PRIORITY SECTORS</h6>
+                                {{-- <div class="dropdown no-arrow">
                                     <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
@@ -183,7 +183,7 @@
                                         <div class="dropdown-divider"></div>
                                         <a class="dropdown-item" href="#">Something else here</a>
                                     </div>
-                                </div>
+                                </div> --}}
                             </div>
                             <!-- Card Body -->
                             <div class="card-body">
@@ -223,7 +223,7 @@
                     <div class="col-lg-12 mb-4">
 
                         <!-- Project Card Example -->
-                        <div class="card shadow mb-4">
+                        {{-- <div class="card shadow mb-4">
                             <div class="card-header py-3">
                                 <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
                             </div>
@@ -261,7 +261,7 @@
                                         aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
 
