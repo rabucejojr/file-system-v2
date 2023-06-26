26ea23dee81af013724b422714fec9363f88111a
rabucejo <rogerhapay@gmail.com>
2023-06-26 16:58:45 +0800

    edit login bg co lor

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                 </a>
             @endif
 
-            <x-primary-button class="ml-3">
+            <x-primary-button class="ml-3 align-center">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
