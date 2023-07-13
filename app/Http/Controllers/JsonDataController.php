4aeeebe5620bea08516c5bc7290445f2f7817cbe
rabucejo <rogerhapay@gmail.com>
2023-07-13 15:27:40 +0800

    filter json data

--- app/Http/Controllers/JsonDataController.php
+++ app/Http/Controllers/JsonDataController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class JsonDataController extends Controller
 {
     //
-    public function fetchData()
+    public function fetchData(Request $request)
     {
-        $jsonData = File::table('files')->select('FileFolder')->get();
-
+        // filter value
+        $filterValue = $request->input('filter');
+        
+        // fetch json data from mysql
+        $jsonData = DB::table('files')->where('FileFolder->FileFolder',$filterValue)->get();
         return response()->json($jsonData);
     }
 }
