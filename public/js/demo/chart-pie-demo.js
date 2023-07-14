45961dec5beff51e57d4c9d0c99a56e7f31dee9c
rabucejo <rogerhapay@gmail.com>
2023-07-14 13:31:04 +0800

    fixed chart data from mysql

--- public/js/demo/chart-pie-demo.js
+++ public/js/demo/chart-pie-demo.js
@@ -7,47 +7,48 @@ $.ajax({
     url: "/json-data",
     method: "GET",
     success: function (response) {
-        // Use the response data to create and render your chart
-        createChart(response);
+        var dataObject = response;
+        var dataArray = Object.keys(dataObject).map(function (key) {
+            return dataObject[key];
+        });
+        // console.log(dataArray);
+        // Pie Chart
+        var ctx = document.getElementById("myPieChart");
+        var myPieChart = new Chart(ctx, {
+            type: "doughnut",
+            data: {
+                labels: ['SETUP','GIA','OTHERS'],
+                datasets: [
+                    {
+                        data: dataArray,
+                        backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc"],
+                        hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf"],
+                        hoverBorderColor: "rgba(234, 236, 244, 1)",
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                tooltips: {
+                    backgroundColor: "rgb(255,255,255)",
+                    bodyFontColor: "#858796",
+                    borderColor: "#dddfeb",
+                    borderWidth: 1,
+                    xPadding: 15,
+                    yPadding: 15,
+                    displayColors: false,
+                    caretPadding: 10,
+                },
+                legend: {
+                    display: false,
+                },
+                cutoutPercentage: 80,
+            },
+        });
     },
     error: function (xhr, status, error) {
         console.error(error);
     },
 });
 
-function createChart(data) {
-    // Pie Chart Example
-    var ctx = document.getElementById("myPieChart");
-    var myPieChart = new Chart(ctx, {
-        type: "doughnut",
-        data: {
-            labels: data.map((item) => item.label),
-            datasets: [
-                {
-                    data: data.map((item) => item.value),
-                    backgroundColor: ["#4e73df", "#1cc88a", "#36b9cc"],
-                    hoverBackgroundColor: ["#2e59d9", "#17a673", "#2c9faf"],
-                    hoverBorderColor: "rgba(234, 236, 244, 1)",
-                },
-            ],
-        },
-        options: {
-            responsive:true,
-            maintainAspectRatio: false,
-            tooltips: {
-                backgroundColor: "rgb(255,255,255)",
-                bodyFontColor: "#858796",
-                borderColor: "#dddfeb",
-                borderWidth: 1,
-                xPadding: 15,
-                yPadding: 15,
-                displayColors: false,
-                caretPadding: 10,
-            },
-            legend: {
-                display: false,
-            },
-            cutoutPercentage: 80,
-        },
-    });
-}
