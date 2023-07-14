78c19fb52c077f85ed0b96b430f7eb15bdefbf40
rabucejo <rogerhapay@gmail.com>
2023-07-14 16:26:41 +0800

    added carousel in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -62,6 +62,34 @@
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExample" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExample" data-slide-to="1"></li>
+                            <li data-target="#carouselExample" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="/img/dost.png" class="d-block w-80" alt="Image 1">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/img/dost.png" class="d-block w-80" alt="Image 2">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="/img/dost.png" class="d-block w-80" alt="Image 3">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- End of Main Content -->
