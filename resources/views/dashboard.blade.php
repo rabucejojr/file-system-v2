e4701d53859c283bc4fa47ad953122cbff420650
rabucejo <rogerhapay@gmail.com>
2023-07-17 18:09:28 +0800

    modified dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,7 +26,7 @@
 
                 <div class="row">
                     <!-- Pie Chart -->
-                    <div class="col-xl-12 col-lg-12">
+                    <div class="col-xl-6 col-lg-6">
                         <div class="card shadow mb-4">
                             <!-- Card Header - Dropdown -->
                             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -63,42 +63,45 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div id="carouselExample" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carouselExample" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExample" data-slide-to="1"></li>
-                            <li data-target="#carouselExample" data-slide-to="2"></li>
-                            <li data-target="#carouselExample" data-slide-to="3"></li>
-                            <li data-target="#carouselExample" data-slide-to="4"></li>
-                            <li data-target="#carouselExample" data-slide-to="5"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="/img/dost.png" class="w-80" alt="Image 1">
+                    <div class="col-xl-6 col-lg-6">
+                        <div id="carouselExample" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExample" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExample" data-slide-to="1"></li>
+                                <li data-target="#carouselExample" data-slide-to="2"></li>
+                                <li data-target="#carouselExample" data-slide-to="3"></li>
+                                <li data-target="#carouselExample" data-slide-to="4"></li>
+                                <li data-target="#carouselExample" data-slide-to="5"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src="/img/dost.png" width="450px" class="d-block mx-auto" alt="Image 1">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/dost1.jpg" width="450px" class="d-block mx-auto" alt="Image 2">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/dost2.jpg" width="450px" class="d-block mx-auto" alt="Image 3">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/dost3.jpg" width="450px" class="d-block mx-auto" alt="Image 4">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/dost4.jpg" width="450px" class="d-block mx-auto" alt="Image 5">
+                                </div>
+                                <div class="carousel-item">
+                                    <img src="/img/dost5.jpg" width="450px" class="d-block mx-auto" alt="Image 6">
+                                </div>
                             </div>
-                            <div class="carousel-item">
-                                <img src="/img/dost1.jpg" class="w-80" alt="Image 2">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/img/dost2.jpg" class="w-80" alt="Image 3">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/img/dost3.jpg" class="w-80" alt="Image 4">
-                            </div>
-                            <div class="carousel-item">
-                                <img src="/img/dost4.jpg" class="w-80" alt="Image 5">
-                            </div><div class="carousel-item">
-                                <img src="/img/dost5.jpg" class="w-80" alt="Image 6">
-                            </div>
+                            <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
-                        <a class="carousel-control-prev" href="#carouselExample" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExample" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
                     </div>
                 </div>
             </div>
