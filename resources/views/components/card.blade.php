dc7c1b94226ea06ed093e504c241130310108907
rabucejo <rogerhapay@gmail.com>
2023-07-31 09:04:30 +0800

    dashboard new layout

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,16 @@
+ <div class="flex flex-cols gap-8">
+     <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+         <img class="w-auto h-90 object-cover" src="{{ $location }}" alt="Card Image">
+         <div class="px-6 py-4">
+             <div class="font-bold text-xl mb-2">
+                 <a href="{{ $link }}" target="_blank"> {{ $title }}</a>
+             </div>
+             <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                 #OneDOST4U
+             </p>
+             <p class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                 {{ $program }}
+             </p>
+         </div>
+     </div>
+ </div>
