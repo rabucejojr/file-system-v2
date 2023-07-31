0a4833ad4f4460a32e4d6dbabf194435b01055e8
rabucejo <rogerhapay@gmail.com>
2023-07-31 09:41:16 +0800

    removed 3rd GIA slot, no PR to be featured

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -147,59 +147,29 @@
                 <div class="flex items-center justify-center p-2">
                     <div class="flex flex-cols gap-8">
                         {{-- first gia featured --}}
-                        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-                            <img class="w-auto h-90 object-cover" src="/img/gia1.jpg" alt="Card Image">
-                            <div class="px-6 py-4">
-                                <div class="font-bold text-xl mb-2"><a href="https://bit.ly/44VPcb5">𝐃𝐎𝐒𝐓 𝐭𝐮𝐫𝐧𝐬
+                        @component('components.card', [
+                            'location' => '/img/gia1.jpg',
+                            'link' => 'https://bit.ly/44VPcb5',
+                            'title' => '𝐃𝐎𝐒𝐓 𝐭𝐮𝐫𝐧𝐬
                                         𝐨𝐯𝐞𝐫 𝐂𝐚𝐬𝐬𝐚𝐯𝐚 𝐏𝐫𝐨𝐜𝐞𝐬𝐬𝐢𝐧𝐠 𝐅𝐚𝐜𝐢𝐥𝐢𝐭𝐲, 𝐬𝐩𝐚𝐫𝐤𝐬
                                         𝐬𝐨𝐜𝐢𝐨𝐞𝐜𝐨𝐧𝐨𝐦𝐢𝐜 𝐠𝐫𝐨𝐰𝐭𝐡 𝐢𝐧 𝐜𝐨𝐧𝐟𝐥𝐢𝐜𝐭-𝐚𝐟𝐟𝐞𝐜𝐭𝐞𝐝
-                                        𝐜𝐨𝐦𝐦𝐮𝐧𝐢𝐭𝐲</a></div>
-                                <p
-                                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                                    #MPEX
-                                </p>
-                                <p
-                                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                                    #ConsultancyService
-                                </p>
-                            </div>
-                        </div>
+                                        𝐜𝐨𝐦𝐦𝐮𝐧𝐢𝐭𝐲',
+                            'program' => '#GIA',
+                        ])
+                        @endcomponent
                         {{-- second gia featured --}}
-                        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-                            <img class="w-auto h-90 object-cover" src="/img/gia2.jpg" alt="Card Image">
-                            <div class="px-6 py-4">
-                                <div class="font-bold text-xl mb-2"><a href="https://bit.ly/44VPcb5">𝐃𝐎𝐒𝐓-𝐒𝐃𝐍
+                        @component('components.card', [
+                            'location' => '/img/gia2.jpg',
+                            'link' => 'https://bit.ly/44VPcb5',
+                            'title' => '𝐃𝐎𝐒𝐓-𝐒𝐃𝐍
                                         𝐟𝐨𝐫𝐠𝐞𝐬 𝐌𝐎𝐀 𝐰𝐢𝐭𝐡 𝐋𝐆𝐔-𝐌𝐚𝐢𝐧𝐢𝐭 𝐭𝐨 𝐩𝐫𝐨𝐦𝐨𝐭𝐞
                                         𝐢𝐧𝐝𝐢𝐠𝐞𝐧𝐨𝐮𝐬 𝐝𝐞𝐥𝐢𝐜𝐚𝐜𝐢𝐞𝐬 𝐭𝐡𝐫𝐨𝐮𝐠𝐡 𝐢𝐧𝐧𝐨𝐯𝐚𝐭𝐢𝐯𝐞
-                                        𝐭𝐞𝐜𝐡𝐧𝐨𝐥𝐨𝐠𝐢𝐞𝐬
-                                    </a></div>
-                                <p
-                                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                                    #MPEX
-                                </p>
-                                <p
-                                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                                    #ConsultancyService
-                                </p>
-                            </div>
-                        </div>
+                                        𝐭𝐞𝐜𝐡𝐧𝐨𝐥𝐨𝐠𝐢𝐞𝐬',
+                            'program' => '#GIA',
+                        ])
+                        @endcomponent
                         {{-- third gia featured --}}
-                        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-                            <img class="w-auto h-90 object-cover" src="/img/dost4.jpg" alt="Card Image">
-                            <div class="px-6 py-4">
-                                <div class="font-bold text-xl mb-2"><a href="https://bit.ly/44VPcb5">𝐃𝐎𝐒𝐓-𝐒𝐃𝐍
-                                        𝐦𝐨𝐧𝐢𝐭𝐨𝐫𝐬 𝐌𝐏𝐄𝐗 𝐜𝐥𝐢𝐞𝐧𝐭𝐬, 𝐬𝐞𝐞𝐬 𝐬𝐢𝐠𝐧𝐢𝐟𝐢𝐜𝐚𝐧𝐭
-                                        𝐢𝐦𝐩𝐫𝐨𝐯𝐞𝐦𝐞𝐧𝐭𝐬</a></div>
-                                <p
-                                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                                    #MPEX
-                                </p>
-                                <p
-                                    class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                                    #ConsultancyService
-                                </p>
-                            </div>
-                        </div>
+                        {{-- vacant entry --}}
                     </div>
                 </div>
                 {{-- End of GIA --}}
